366dddf65b6e13ed52a2bab124897d385ea158a7
Ari D. Simon <47069593+aridsimon@users.noreply.github.com>
2025-08-28 11:32:20 -0400

    Create filters.php

---
+++ filters.php
@@ -0,0 +1,21 @@
+<?php
+/** adds text to display when a facet is empty **/
+add_action( 'facetwp_scripts', function() { ?>
+<script>
+(function($) {
+    $(function() {
+        FWP.hooks.addAction('facetwp/loaded', function() {
+            $('.facetwp-facet').each(function() {
+                var content = $(this).html();
+                if ('' == content) {
+                    $(this).append("<p class='no-results-message' style='color: #fff;''>Nothing found matching the selected criteria.</p>");
+                }				
+            });
+        }, 1000 );
+    });
+})(fUtil);
+</script>
+<?php }, 100 );
+
+
+?>
